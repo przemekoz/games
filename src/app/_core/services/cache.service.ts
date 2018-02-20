4442beb5c95c12dcef56c06a008a91b1689866c4
Przemek Kozinski <przemekoz@o2.pl>
2018-02-20 23:10:55 +0100

    cache service

--- src/app/_core/services/cache.service.ts
+++ src/app/_core/services/cache.service.ts
@@ -4,7 +4,8 @@ import { of } from 'rxjs/observable/of';
 import { ListParam, List } from '../../interfaces/list';
 
 interface CacheItem {
-    key: 'string';
+    key: string;
+    id: string;
     created: number;
     item: any | any[];
     total?: number;
@@ -17,7 +18,7 @@ export class CacheService {
 
     private cache: CacheItem[] = [];
 
-    private find(key: string): CacheItem {
+    private find(key: string, params: ListParam | string): CacheItem {
         return this.cache.find(item => item.key === key && this.isActive(item));
     }
 
@@ -29,49 +30,62 @@ export class CacheService {
         const found = this.cache.filter(item => this.isActive(item));
         found.forEach(item => {
             this.cache.splice(this.cache.indexOf(item), 1);
-        })
+        });
     }
 
-    get(key: string, observable, params: ListParam): Observable<List> | any {
-        const cacheItem = this.find(key);
-        if (cacheItem) {
-            if (cacheItem.total) {
-                return of({ items: cacheItem.item, total: cacheItem.total });
-            } else {
-                return of(cacheItem.item);
-            }
-        } else {
-
-        }
-
-        // .subscribe(response => {
-        //     this.games.push(...response.items);
+    private getResult(cacheItem: CacheItem) {
         //     return of({ items: this.games, total: this.games.length });
-        // })
+        return cacheItem.item;
     }
 
+    get(key: string, observable, params: ListParam | string): Observable<List> | any {
+        let cacheItem = this.find(key, params);
+        if (cacheItem) {
+            return of(this.getResult(cacheItem));
+        } else {
+            observable.subscribe(response => {
+                cacheItem = this.prepare(key, this.getId(params), response);
+                this.removeOld();
+                this.cache.push(cacheItem);
+                return of(this.getResult(cacheItem));
+            });
+        }
+    }
 
+    private prepare(key: string, id: string, result: any): CacheItem {
+        return {
+            key: key,
+            id: id,
+            created: Date.now(),
+            item: result.item
+        };
+    }
 
-    /*
-    
+    private getId(params: ListParam | string): string {
+        if (typeof params === 'string') {
+            return params;
+        } else {
+            return this.parseListParams(params);
+        }
+    }
 
-    
-    
-    */
+    private parseListParams(params: ListParam): string {
+        return 'some of string';
+    }
 
     /*
     offset=0 limit=20
         games.slice(0, 0 * 20 + 20)
-
+    
     offset=5 limit=20
         games.slice(5*20, 5*20 + 20)
-
+    
     offset=2 limit=5
         games.slice(2*5, 2*5 + 5)
-
+    
     let start = offset * limit;
     games.slice(start, start + limit)
-
+    
     */
 
     /*
@@ -81,26 +95,21 @@ export class CacheService {
         {3,'c'}
         {5,'e'}
         {7,'h'}
-
+    
     offset=0&limit=20&sort=+title
         {15,'z'}
         {2,'b'}
         {3,'c'}
         {1,'a'}
         {7,'h'}
-
+    
     offset=1&limit=20&sort=+title
         {8,'k'}
         {10,'n'}
         {11,'m'}
         {17,'o'}
         {22,'y'}
-
+    
     */
-
-
     constructor() { }
-
-
-
 }
